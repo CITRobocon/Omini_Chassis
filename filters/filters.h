a61a962b3bf790dbd6c2753e45dc7b74a64da940
sanoshohei <38240672+sanoshohei@users.noreply.github.com>
2019-11-03 12:16:45 +0900

    Update filters.h

--- filters/filters.h
+++ filters/filters.h
@@ -19,7 +19,7 @@ typedef struct{
 
 	float k;
 	float base[1];
-}filters_infinite_response_filter_set;
+}infinite_response_filter_set;
 
 typedef struct{
 	struct{
@@ -28,7 +28,7 @@ typedef struct{
 	}io_ptr;
 
 	size_t size;
-}filters_finite_response_filter_set;
+}finite_response_filter_set;
 
 // high level filter sets
 typedef struct{
@@ -40,27 +40,30 @@ typedef struct{
 
 	float k;
 	float base[2];
-}filters_complementary_filter_set;
+}complementary_filter_set;
 
 
 /* make filter object */
 
-filters_infinite_response_filter_set filters_infr_filter_set_create(float*, float*, float);
+infinite_response_filter_set infinite_response_filter_set_create(float*, float*, float);
 
-filters_finite_response_filter_set filters_fr_filter_set_create(float*, float*, size_t);
+finite_response_filter_set finite_response_filter_set_create(float*, float*, size_t);
 
-filters_complementary_filter_set filters_complementary_filter_set_create(float*, float*, float*, float);
+complementary_filter_set complementary_filter_set_create(float*, float*, float*, float);
 
 
 /* filters */
 
 // RC filter: a basic infinite response low pass filter
-void filters_RC_filter(filters_infinite_response_filter_set*);
+void RC_filter(infinite_response_filter_set*);
 
 // LC filter: a basic infinite response high pass filter
-void filters_LC_filter(filters_infinite_response_filter_set*);
+void LC_filter(infinite_response_filter_set*);
 
 // moving average filter: a finite response low path filter
-void filters_moving_average_filter(filters_finite_response_filter_set*);
+void moving_average_filter(finite_response_filter_set*);
 
-void filters_complementary_filter(filters_complementary_filter_set*);
+void complementary_filter(complementary_filter_set*);
+
+
+#endif /* FILTERS_FILTERS_H_ */
