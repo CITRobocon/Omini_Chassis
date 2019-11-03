e6c6a0a6850bd7cb73fb6c1ff749c2f2f54563f6
sanoshohei <38240672+sanoshohei@users.noreply.github.com>
2019-11-03 12:16:05 +0900

    Update filters.c

--- filters/filters.c
+++ filters/filters.c
@@ -9,8 +9,8 @@
 
 #include "filters.h"
 
-filters_infinite_response_filter_set filters_infr_filter_set_create(float *in_ptr, float *out_ptr, float k){
-	filters_infinite_response_filter_set set;
+infinite_response_filter_set infinite_response_filter_set_create(float *in_ptr, float *out_ptr, float k){
+	infinite_response_filter_set set;
 
 	set.io_ptr.in = in_ptr;
 	set.io_ptr.out = out_ptr;
@@ -20,8 +20,8 @@ filters_infinite_response_filter_set filters_infr_filter_set_create(float *in_pt
 	return set;
 }
 
-filters_finite_response_filter_set filters_fr_filter_set_create(float *in_ptr, float *out_ptr, size_t size){
-	filters_finite_response_filter_set set;
+finite_response_filter_set finite_response_filter_set_create(float *in_ptr, float *out_ptr, size_t size){
+	finite_response_filter_set set;
 
 	set.io_ptr.in = in_ptr;
 	set.io_ptr.out = out_ptr;
@@ -30,8 +30,8 @@ filters_finite_response_filter_set filters_fr_filter_set_create(float *in_ptr, f
 	return set;
 }
 
-filters_complementary_filter_set filters_complementary_filter_set_create(float *out_ptr, float *in_high_ptr, float *in_low_ptr, float k){
-	filters_complementary_filter_set set;
+complementary_filter_set complementary_filter_set_create(float *out_ptr, float *in_high_ptr, float *in_low_ptr, float k){
+	complementary_filter_set set;
 
 	set.io_ptr.in_high = in_high_ptr;
 	set.io_ptr.in_low = in_low_ptr;
@@ -47,19 +47,19 @@ filters_complementary_filter_set filters_complementary_filter_set_create(float *
 /* filters */
 
 // RC filter: a basic infinite response low pass filter
-void filters_RC_filter(filters_infinite_response_filter_set *set){
+void RC_filter(infinite_response_filter_set *set){
 	set->base[0] = set->k * *(set->io_ptr.in) + (1.0 - set->k) * set->base[0];
 	*(set->io_ptr.out) = set->base[0];
 }
 
 // LC filter: a basic infinite response high pass filter
-void filters_LC_filter(filters_infinite_response_filter_set *set){
+void LC_filter(infinite_response_filter_set *set){
 	set->base[0] = set->k * *(set->io_ptr.in) + (1.0 - set->k) * set->base[0];
 	*(set->io_ptr.out) = *(set->io_ptr.in) - set->base[0];
 }
 
 // moving average filter: a finite response low path filter
-void filters_moving_average_filter(filters_finite_response_filter_set *set){
+void moving_average_filter(finite_response_filter_set *set){
 	*(set->io_ptr.out) = *(set->io_ptr.in + set->size - 1);
 
 	for (int i = set->size - 1; i > 0; i--){
@@ -70,7 +70,7 @@ void filters_moving_average_filter(filters_finite_response_filter_set *set){
 	*(set->io_ptr.out) /= (float)set->size;
 }
 
-void filters_complementary_filter(filters_complementary_filter_set *set){
+void complementary_filter(complementary_filter_set *set){
 	set->base[0] = set->k * *(set->io_ptr.in_high) + (1.0 - set->k) * set->base[0];
 	float out_high = *(set->io_ptr.in_high) - set->base[0];
 
